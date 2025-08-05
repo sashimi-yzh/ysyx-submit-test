129e19afe91a377cea579ca68d822caabc0f01a5
Zihao Yu <yuzihao@ict.ac.cn>
2025-08-06 00:03:55 +0800

    ci: use random string for monitoring

--- monitor.py
+++ monitor.py
@@ -6,17 +6,19 @@ import pty, os, sys
 parser = argparse.ArgumentParser()
 parser.add_argument('--timeout', type=int, default=3600)
 parser.add_argument('--good', type=str, default="HIT GOOD TRAP")
+parser.add_argument('--middle', type=str, default="MicroBench PASS")
 parser.add_argument('--bad', type=str, default="HIT BAD TRAP")
 parser.add_argument("cmd", nargs=argparse.REMAINDER)
 args = parser.parse_args()
 
-keys = {"success": args.good, "error": args.bad}
+keys = {"good": args.good, "middle": args.middle, "bad": args.bad}
 state = "empty"
+state_rtt = "empty"
             # input                  string for success
 rtt_input = [('help',                 'am_hello'),
              ('am_hello',             'Hello, AbstractMachine!'),
              ('am_microbench',        'MicroBench PASS'),
-             ('echo "HIT GOOD TRAP"', 'HIT GOOD TRAP')]
+             (f'echo "{args.good}"',  args.good)]
 
 buffer = bytearray()
 def read_line(fd, buffer_size=1024):
@@ -54,6 +56,7 @@ def read_line(fd, buffer_size=1024):
 
 def monitor_output(proc, result_queue):
     global state
+    global state_rtt
     global master
     global rtt_input
     rtt_ans = ""
@@ -62,19 +65,21 @@ def monitor_output(proc, result_queue):
         if line == None:
             break
         print(line)
-        if state == "rtt-monitor":
+        if state_rtt == "rtt-monitor":
             if rtt_ans in line:
-                state = "empty"
+                state_rtt = "empty"
         else:
             if line == "msh />" and rtt_input != []:
                 proc.stdin.write(rtt_input[0][0] + '\n')
                 proc.stdin.flush()
                 rtt_ans = rtt_input[0][1]
-                state = "rtt-monitor"
+                state_rtt = "rtt-monitor"
                 rtt_input = rtt_input[1:]
-            for s, k in keys.items():
-                if k in line:
+        for s, k in keys.items():
+            if k in line:
+                if not (s == "good" and state != "middle"):
                     state = s
+                if state == "good" or state == "bad":
                     result_queue.put("match")
                     proc.kill()
                     return
@@ -95,9 +100,12 @@ except queue.Empty:
     proc.kill()
     print("Kill the process due to timeout")
 
+if state == "good":
+    print(sys.argv[0] + ": Keyword detected, OK!")
+
 if state == "empty":
     print(sys.argv[0] + ": Keyword not detected")
     exit(1)
-if state == "error":
+if state == "bad":
     print("sys.argv[0]" + ": Detect bad trap")
     exit(1)
